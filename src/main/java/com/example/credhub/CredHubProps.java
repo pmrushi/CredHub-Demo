7180f7a54efa0efe468f22802a2f96775e179aa4
Rushi Pamu <rushi.p@imaginea.com>
2021-05-05 22:51:45 +0530

    updated jar

--- src/main/java/com/example/credhub/CredHubProps.java
+++ src/main/java/com/example/credhub/CredHubProps.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("vcap.services.mycredhub.credentials")
+@ConfigurationProperties("vcap.services.credhub.credentials")
 //add the name of your PCF service here to replace "mycredhub"
 public class CredHubProps {
     public String mysecretkey; //add whatever properties you need in here that are stored in CredHub
